dc7a8e2ca5a307be2f12ae5a19b8dd2076cca4c9
Sandeep Zechariah George <sandeepzgk@gmail.com>
2023-01-28 13:01:23 -0800

    magic and this seems to work i donno how

--- src/components/BezierCurveEditor/BezierCurveEditor.tsx
+++ src/components/BezierCurveEditor/BezierCurveEditor.tsx
@@ -45,7 +45,7 @@ const defaultStateValue: IState = {
   movingEndHandle: false,
   movingStartHandleStart: { x: 0, y: 0 },
   movingEndHandleStart: { x: 0, y: 0 },
-  leftHandlePosition: .5, // 0=bottom, 1=top
+    leftHandlePosition: 0.0,
 }
 
 export class BezierCurveEditor extends React.Component<IProps, IState> {
@@ -68,9 +68,7 @@ export class BezierCurveEditor extends React.Component<IProps, IState> {
     public state: IState = {
       ...defaultStateValue
     }
-    public get leftHandlePosition() {
-        return this.state.leftHandlePosition;
-    }
+
 
     //#region getters
     private get width() {
@@ -83,7 +81,8 @@ export class BezierCurveEditor extends React.Component<IProps, IState> {
     }
 
     private get startCoordinate() {
-        return [0, this.height-this.leftHandlePosition*this.height];
+        const { leftHandlePosition } = this.props as PropsWithDefaults;
+        return [0, this.height-leftHandlePosition*this.height];
     }
 
     private get endCoordinate() {
