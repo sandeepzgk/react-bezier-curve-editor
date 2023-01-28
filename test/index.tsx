e22af936ee075257bf08b811e77e2c26b2233d06
Sandeep Zechariah George <sandeepzgk@gmail.com>
2023-01-28 11:53:13 -0800

    adding rc-slider

--- test/index.tsx
+++ test/index.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BezierCurveEditor } from '../lib';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css'
+
+const verticalstyle = { height: 600};
+const horizontalstyle = { width: 600};
+const rowC ={"display":"flex", "flexDirection":"row"};
 
 interface IState {
 
@@ -10,13 +16,27 @@ class App extends React.Component<{}, IState> {
     public state = {
 
     }
+    hChange = (value) => {
+        console.log(value);
+      }
+
+      vChange = (value) => {
+        console.log(value);
+      }
 
     public render() {
         return (
             <main>
+                
+           <div style={rowC}>
+           <Slider vertical={true} style={verticalstyle} onChange={this.vChange} />
+           <div>
                 <BezierCurveEditor
-                    size={300}
+                    size={1200}
                 />
+                <Slider style={horizontalstyle}  onChange={this.hChange} />
+                </div>
+                </div>
             </main>
         );
     }
